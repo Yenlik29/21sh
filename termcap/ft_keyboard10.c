c9ed45d1014346dfab54cc926b8c9a6c4c00cb1b
Yenlik BOKINA <ybokina@e2r12p7.unit.ua>
2018-11-10 23:43:14 +0200

    h

--- termcap/ft_keyboard10.c
+++ termcap/ft_keyboard10.c
@@ -20,20 +20,17 @@ void				shift_right_end(t_shell *shell)
 		shell->position++;
 	}
 	clear_till_begin(shell);
-	shell->position = 0;
 	shell->history = shell->history->next;
 	print_line(shell);
 	set_cursor(shell);
-	// ft_strclr(shell->unparsed_com);
-	// ft_strncat(shell->unparsed_com, shell->history->record, ft_strlen(shell->history->record));
+	ft_strclr(shell->unparsed_com);
+	ft_strncat(shell->unparsed_com, shell->history->record, ft_strlen(shell->history->record));
 }
 
 void				shift_right_norm(t_shell *shell)
 {
 	int position;
 
-	// printf("[%s->%s]\n", shell->history->record, shell->history->next->record);
-	// printf("[%d:%d->%s]\n", shell->position, shell->length, shell->history->record);
 	position = shell->position;
 	while (shell->position != shell->length)
 	{
@@ -43,7 +40,6 @@ void				shift_right_norm(t_shell *shell)
 	clear_till_begin(shell);
 	shell->history = shell->history->next;
 	print_line(shell);
-	// printf("[%s->%s]\n", shell->unparsed_com, shell->history->record);
 	set_cursor(shell);
 	while (shell->position != position)
 	{
@@ -65,4 +61,5 @@ void				shift_right(t_shell *shell)
 		shift_right_end(shell);
 	else
 		shift_right_norm(shell);
+	// printf("[%s->%s]\n", shell->unparsed_com, shell->history->record);
 }
