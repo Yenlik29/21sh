a8699d372611f0c1b9e30b8b0fb8f7ba248b76a9
Yenlik BOKINA <ybokina@e1r3p2.unit.ua>
2018-11-15 09:50:39 +0200

    hh

--- termcap/ft_keyboard15.c
+++ termcap/ft_keyboard15.c
@@ -17,23 +17,24 @@ void				multi_clear_till_begin(t_shell *shell)
 	int 			row;
 	struct winsize 	sz;
 
-	row = 0;
 	ioctl(0, TIOCGWINSZ, &sz);
+	row = 0;
 	while (shell->position)
 	{
 		row = row_find(row, shell);
 		if (L_L)
 		{
-			printf("[%c->%d]\n", shell->history->record[shell->position], shell->position + 4);
-			// go_up(shell, sz);
-			// write(1, "\n", 1);
-			// exit(0);
+			go_up(shell, sz);
+			tputs(tgetstr("dc", NULL), 1, re_putchar);
 		}
-		tputs(tgetstr("dc", NULL), 1, re_putchar);
-		tputs(tgetstr("le", NULL), 1, re_putchar);
-		shell->position--;
+		else
+		{
+			in_line_left(shell);
+			tputs(tgetstr("dc", NULL), 1, re_putchar);
+		}
 	}
-	// printf("[%d->%c]\n", shell->position, shell->history->record[shell->position]);
+	shell->position = 0;
+	shell->length = ft_strlen(shell->history->record);
 }
 
 void				multi_begin_history_add(t_shell *shell, uint64_t ch)
@@ -48,25 +49,21 @@ void				multi_begin_history_add(t_shell *shell, uint64_t ch)
 	end_key(shell);
 	set_cursor(shell);
 	multi_clear_till_begin(shell);
-	// shell->position = 0;
-	// shell->length = 0;
-	// shell->position = shell->length + 1;
-	// ft_strclr(shell->history->record);
-	// ft_strncat(shell->history->record, hey, ft_strlen(hey));
-	// ft_strncat(shell->history->record, tmp, ft_strlen(tmp));
-	// print_line(shell);
-	// while (shell->position != 1)
-	// {
-	// 	tputs(tgetstr("le", NULL), 1, re_putchar);
-	// 	shell->position--;
-	// }
-	// // shell->position = 1;
-	// shell->length = ft_strlen(shell->history->record);
-	// ft_strclr(shell->unparsed_com);
-	// ft_strncat(shell->unparsed_com, shell->history->record, ft_strlen(shell->history->record));
-	// free(tmp);
-	// free(hey);
-	// printf("[%d->%d:%zu->%s]\n", shell->position, shell->length, ft_strlen(shell->history->record), shell->history->record);
+	ft_strclr(shell->history->record);
+	ft_strncat(shell->history->record, hey, ft_strlen(hey));
+	ft_strncat(shell->history->record, tmp, ft_strlen(tmp));
+	print_line(shell);
+	set_cursor(shell);
+	while (shell->position != 1)
+	{
+		tputs(tgetstr("le", NULL), 1, re_putchar);
+		shell->position--;
+	}
+	shell->length = ft_strlen(shell->history->record);
+	ft_strclr(shell->unparsed_com);
+	ft_strncat(shell->unparsed_com, shell->history->record, ft_strlen(shell->history->record));
+	free(tmp);
+	free(hey);
 }
 
 void				norm_begin_history_add(t_shell *shell, uint64_t ch)
