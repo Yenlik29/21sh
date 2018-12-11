a852b97a8e0557f5ad3551b30a1bde9020091abd
Yenlik BOKINA <ybokina@e3r5p10.unit.ua>
2018-12-11 14:31:46 +0200

    h

--- ft_21sh.h
+++ ft_21sh.h
@@ -6,7 +6,7 @@
 /*   By: ybokina <ybokina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/23 12:12:06 by ybokina           #+#    #+#             */
-/*   Updated: 2018/12/10 23:54:19 by ybokina          ###   ########.fr       */
+/*   Updated: 2018/12/11 14:10:14 by ybokina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -278,7 +278,7 @@ void        s_quote(t_lexer **tokens, int *j, t_shell *shell, int *i);
 
 void		parse(t_tokens *tokens);
 int			correct_pre_post(t_tokens *tokens);
-int 		correct_operators(t_tokens *tokens);
+void 		correct_operators(t_tokens *tokens);
 
 // ASTnode		*command_line(t_tokens **tokens);
 
--- parser/parser_init.c
+++ parser/parser_init.c
@@ -6,13 +6,13 @@
 /*   By: ybokina <ybokina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 13:06:12 by ybokina           #+#    #+#             */
-/*   Updated: 2018/12/10 23:55:31 by ybokina          ###   ########.fr       */
+/*   Updated: 2018/12/11 14:29:15 by ybokina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_21sh.h"
 
-int 				correct_operators(t_tokens *tokens)
+void 				correct_operators(t_tokens *tokens)
 {
 	t_tokens *ret;
 	t_tokens *tmp;
@@ -34,28 +34,32 @@ int 				correct_operators(t_tokens *tokens)
 			tmp->mistake = 0;
 		tmp = tmp->next;
 	}
-	while (ret)
-	{
-		if (ret->mistake == 1)
-			return (1);
-		ret = ret->next;
-	}
-	return (0);
 }
 
-int					correct_pre_post(t_tokens *tokens)
+int						correct_pre_post(t_tokens *tokens)
 {
 	t_tokens *temp;
 
 	temp = tokens;
 	while (temp)
 	{
-		printf("[%s->%d]\n", temp->info, temp->type);
-		if ((temp->type == 124 && !temp->prev))
+		if ((temp->type == 124 && !temp->prev) || (temp->type == 38 && !temp->prev))
+		{
+			ft_putstr_fd("21sh: syntax error\n", 1);
+			temp->mistake = 1;
+		}
+		else if (temp->type == 124 && temp->next->type != -1)
 		{
 			ft_putstr_fd("21sh: syntax error\n", 1);
 			return (1);
 		}
+		else if ((temp->type == 62 || temp->type == 60 || temp->type == 140 || temp->type == 142 || temp->type == 144 || temp->type == 146 || temp->type == 148) && temp->next->type != -1)
+		{
+			ft_putstr_fd("21sh: syntax error\n", 1);
+			return (1);
+		}
+		else
+			temp->mistake = 0;
 		temp = temp->next;
 	}
 	return (0);
@@ -66,6 +70,7 @@ void				parse(t_tokens *tokens)
 	t_tokens	*temp;
 
 	temp = tokens;
-	if (!(correct_operators(temp)) || !(correct_pre_post(temp)))
+	correct_operators(temp);
+	if (correct_pre_post(temp) == 1)
 		return ;
 }
