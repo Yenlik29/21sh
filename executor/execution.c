e9c3c3ab70109ac73fedd589bde9face82071495
Yenlik BOKINA <ybokina@e1r3p3.unit.ua>
2018-12-15 13:55:52 +0200

    h

--- executor/execution.c
+++ executor/execution.c
@@ -6,18 +6,27 @@
 /*   By: ybokina <ybokina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 13:17:22 by ybokina           #+#    #+#             */
-/*   Updated: 2018/12/15 13:46:41 by ybokina          ###   ########.fr       */
+/*   Updated: 2018/12/15 13:55:12 by ybokina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_21sh.h"
 
+void                    exec_pipe(t_ast *ast, t_shell *shell, char **w_splited)
+{
+    printf("heey PIPE!\n");
+    ast = NULL;
+    shell = NULL;
+    w_splited = NULL;
+    // if (!ast->left)
+    //     pipeline(ast, ast->parent->right);
+    // exec_pipe(ast->left, shell, w_splited);
+}
+
 void                    exec_semi(t_ast *ast, t_shell *shell, char **env, char **w_splited)
 {
     execution(ast->left, shell, env, w_splited);
-    // printf("!\n");
     execution(ast->right, shell, env, w_splited);
-    // printf("!\n");
 }
 
 void                    execution(t_ast *ast, t_shell *shell, char **env, char **w_splited)
@@ -27,7 +36,7 @@ void                    execution(t_ast *ast, t_shell *shell, char **env, char *
     else if (ast->type == T_SEMI)
         exec_semi(ast, shell, env, w_splited);
     else if (ast->type == T_PIPE)
-        printf("PIPE!\n");
+        exec_pipe(ast, shell, w_splited);
     else if (ast->type != T_PIPE && ast->type != T_SEMI)
     {
         w_splited = split_word(ast->tokens->info);
